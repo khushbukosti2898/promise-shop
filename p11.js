e9fbf1e7951dd621fcb1f7d84694ace1c96af943
bacancy <khushbu.kosti@bacancytechnology.com>
2020-01-20 18:56:20 +0530

    commit promise3

--- p11.js
+++ p11.js
@@ -1,24 +1,27 @@
 function all(a1,a2){
-    new Promise((resolve,reject) => {
+    var p=new Promise((resolve,reject) => {
         counter=0;
         arr=[];
-        a1.then((counter)=>{
-            arr[0]=counter;
+        a1.then((count)=>{
+            arr[0]=count;
             counter++;
             
-            if(counter>2)
+            if(counter>=2){
             resolve(arr);
+            }
             
         });
-        a2.then((counter)=>{
-            arr[1]=counter;
+        a2.then((count)=>{
+            arr[1]=count;
             counter++;
             
-            if(counter>2)
+            if(counter>=2){
             resolve(arr);
+        }
            
         });
     })
+    return p; 
 }
 all(getPromise1(), getPromise2())
   .then(console.log);            
--- p8.js
+++ p8.js
@@ -4,8 +4,8 @@ function attachTitle(value){
 
 /* new Promise((resolve,reject)=>{
     resolve("MANHATTAN");
-}).then((value)=>attachTitle(value)).then(console.log);
- */
+}).then((value)=>attachTitle(value)).then(console.log); */
+
 
 new Promise((resolve,reject)=>{
     resolve("MANHATTAN");
